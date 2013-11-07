9a8d8ffc4e322af0bb25ca1c8d53cc4719474ede
Igor Trakhov <igor.trakhov@gmail.com>
2013-11-07 04:19:42 +0400

    added poisson, laplace and gauss

--- stories/helpers.rb
+++ stories/helpers.rb
@@ -7,8 +7,24 @@ def write(n, name)
 		file.write generate
 	end
 	file.close
+	puts 'done'
 end
 
 def sign
 	rand < 0.5 ? 1 : -1
+end
+
+def lgeq
+	list = [
+		'ровно',
+		'не менее',
+		'не более',
+		'хотя бы',
+		'по крайней мере',
+		'по меньшей мере',
+		'более',
+		'менее',
+	]
+
+	list.shuffle.pop
 end
--- stories/prob/help_04.rb
+++ stories/prob/help_04.rb
@@ -2,30 +2,82 @@
 
 require '../helpers'
 
-puts sign
-
-def near_np(np, npq)
-	deltas = (1..5).map { |a| a / 20.0 }.shuffle
-	Array.new(2) { np + sign * (deltas.pop * np).round }.sort
+def country
+	list = [
+		'Швамбрании',
+		'Уганды',
+		'Французской Полинезии',
+		'Штатов Египетского Дождя',
+		'Северной Нарнии',
+		'Левобережья Евфрата',
+		'Островов Крокодилова Клюва',
+		'Атлантиды',
+		'Мордора',
+		'Студландии-2'
+	]
+	list.shuffle.pop
 end
 
-# puts near_np 50
+def date
+	months = [
+		['января', 31],
+		['февраля', 28],
+		['марта', 31],
+		['апреля', 30],
+		['мая', 31],
+		['июня', 30],
+		['июля', 31],
+		['августа', 31],
+		['сентября', 30],
+		['октября', 31],
+		['ноября', 30],
+		['декабря', 31],
+	]
+
+	m = months.shuffle.pop
+
+	"#{rand(1..m[1])} #{m[0]}"
+end
 
 
-def laplace
+def near_np(n, p)
+	arr = []
+	m = 0
+	np, sq = [n * p, Math.sqrt(n * p * (1 - p))]
+	deltas = (2..15).map { |a| (a * np / 100.0).round }
+
+	2.times do |i|
+		j = 1
+		until ((m - np) / sq).abs < 3 && !arr.include?(m.round)
+			m = np + sign * deltas.shuffle.pop
+			j += 1
+			if j > 100
+				break
+			end
+		end
+		arr << m.round
+	end
+	arr.sort! << (np + sign * deltas.shuffle.pop).round
+	arr
+end
+
+def create_laplace
 	squares = (4..30).map { |a| a ** 2 }
 	probs = (10..90).map { |a| a / 100.0 }
 
-	out = []
+	out = Hash.new { |hash, key| hash[key] = [] }
 
 	probs.each do |p|
 		squares.each do |sq|
 			n = sq / (p - p ** 2)
-			if n % 10 < 0.001 and !out.include? [n.round, p]				
-				out << {
-					n: n.round,
-					p: p,
-					# m: n * p + (rand < 0.5 ? rand())
+			if n % 10 < 0.001	
+				near = near_np n, p
+				out[n.round] << {
+					# n: n.round,
+					p: "$#{p.to_s.sub '.', '{,}'}$",
+					a: near[0],
+					b: near[1],
+					m: near[2]
 				}
 			end
 		end
@@ -34,71 +86,69 @@ def laplace
 	out
 end
 
-# print laplace
+# create_laplace.each do |hash|
+# 	puts hash
+# end
 
 
-def poisson
-	probs = (1..50).map { |a| a / 1000.0 }
-	lambdas = (1..9).map { |a| a / 10.0 } + (1..11).to_a
-end
+def create_poisson(laplace)
+	probs = (10..500).map { |a| a / 10000.0 }
+	lambdas = (3..9).map { |a| a / 10.0 } + (1..9).to_a
 
+	# quantities = laplace.map { |hash| hash[:n] }.uniq.shuffle
+	quantities = laplace.keys.uniq
 
-def params
-	squares = (4..30).map { |a| a**2 }
-	poisson_probs = (1..50).map { |a| a/1000.0 }
-	laplace_probs = (1..9).map { |a| a/10.0 }
+	out = Hash.new { |hash, key| hash[key] = [] }
 
-	# laplace = Hash.new { |h, k| h[k] = [] }
-	laplace = []
-
-	laplace_probs.each do |p|
-		squares.each do |sq| 
-			n = sq / (p - p**2)
-			if n % 10 < 0.001 and !laplace.include? [p, n.round]
-				laplace << [n.round, p]
-			# if n % 10 < 0.001 and !laplace[p].include? n.round
-			# 	laplace[p] << n.round
+	quantities.each do |n|	
+		probs.shuffle.each do |p|
+			lambda = n * p
+			if (lambdas.include? n * p)
+				out[n] << {
+					p: "$#{p.to_s.sub '.', '{,}'}$",
+					q: "$#{(1-p).to_s.sub '.', '{,}'}$",
+					m: rand(2..5),
+					proc: "$#{(p * 100).round(8).to_s.sub '.', '{,}'}\\%$"
+				}
+			else
 			end
 		end
 	end
 
-	poisson = Hash.new { |h, k| h[k] = [] }
-	# poisson = []
-	
-	lambdas = (1..9).map { |a| a/10.0 } + (1..11).to_a
-	# poisson_quantities = laplace.values.reduce { |arr1, arr2| arr1 + arr2 }.uniq
-	poisson_quantities = laplace.map { |arr| arr[1] }.uniq
-	poisson_probs.each do |p|
-		poisson_quantities.each do |n|
-			if lambdas.include? n * p
-				# poisson[p] << n
-				# poisson << [p, n]
-				poisson[n] << p
-			end
-		end
-	end
-
-	{laplace: laplace, poisson: poisson}
+	out
 end
 
-# puts params
+# puts create_poisson create_laplace
+
+
 
 def generate
 	tmpl = open("./tmpl_04") { |file| file.readlines.shuffle!.pop }
+
+	params = create_laplace
+	poissoned = create_poisson params
+
+	n = poissoned.keys.shuffle.pop
+
+	laplace = params[n].shuffle.pop
+	poisson = poissoned[n].shuffle.pop
+
+	tmpl % {
+		n: n,
+		laplaceP: laplace[:p],
+		laplaceA: laplace[:a],
+		laplaceB: laplace[:b],
+		gaussM: laplace[:m],
+		poissonP: poisson[:p],
+		poissonQ: poisson[:q],
+		poissonM: poisson[:m],
+		poissonPr: poisson[:proc],
+		lgeq: lgeq,
+		country: country,
+		date: date
+	}
 end
 
 # puts generate
 
-
-
-
-# %{n}
-# %{poissonP}
-# %{laplaceP}
-# %{lgeq} %{poissonM}
-# %{gaussM}
-# %{laplaceA} %{laplaceB}
-
-
-
-# write 50 '04.txt'
+write 50, '04.txt'
