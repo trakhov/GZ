343c0a036a47afd9b64c069d777c99f348997f69
Igor Trakhov <igor.trakhov@gmail.com>
2013-11-09 22:32:40 +0400

    modified bash script

--- kr_prob_1.sh
+++ kr_prob_1.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 cd stories/prob
-
 for file in gen*; do
 	ruby $file;
 done
 
-echo
-echo all tasks successfully generated
-
 cd ../..
-
 ruby kr_prob_1.rb
-
-echo
-echo data file successfully written
-
 latex -output-directory output -output-format pdf kr_prob_1.tex
 
-echo
-echo done
+cd output
+rm *.out *.aux 
+mv kr_prob_1.log .kr_prob_1.log
 
-exit 1
+exit 0
